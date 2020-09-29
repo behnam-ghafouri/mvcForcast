c6c480f39068db56ca6f10cf1c515bc33cc42170
Behnam Ghafouri <bghafouri@questwindows.com>
2020-09-29 12:51:01 -0400

    using dictionary

--- WebApplication1/Models/StylesForSendToFront1.cs
+++ WebApplication1/Models/StylesForSendToFront1.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace WebApplication1.Models
 {
     public class StylesForSendToFront1
-    {               
+    {
         public SwingDoor swingdoor_ { get; set; }        
         public AwningAll awningall_ { get; set; }
         public  AwningF92 awningf92_ { get; set; }
@@ -16,20 +16,18 @@ namespace WebApplication1.Models
 
         public StylesForSendToFront1(List<Job> input,string[] StylesNeedForecast)
         {
-            Style styleobject = new Style();
 
+            Style styleobject = new Style();
+           
+
+            
             this.swingdoor_ = new SwingDoor(input, styleobject);
             this.awningall_ = new AwningAll(input, styleobject);
             this.awningf92_ = new AwningF92(input, styleobject);
             this.awningm90_ = new AwningM90(input, styleobject);
             this.awningmetra_ = new Awningmetra(input, styleobject);
             this.awningquest_ = new Awningquest(input, styleobject);
-
-
-
-            
-        }
-
-        
+           
+        }        
     }
 }
